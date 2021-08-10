87c7889cb8aead725e0b594736b45eca5cd29164
Ssosiashvili <sosiashvilisoso@gmail.com>
2021-08-11 03:07:55 +0400

    fix

--- Homework 20/script.js
+++ Homework 20/script.js
@@ -1,24 +1,25 @@
 let html = document.querySelector('html');
 let navbarArray = document.querySelectorAll('a');
 
-for (let i = 0; i < navbarArray.length; i ++) 
-navbarArray[i].addEventListener('click', function (e){
-	let sectionId = navbarArray[i].getAttribute('href');
-	console.log(sectionId);
+for (let i = 0; i < navbarArray.length; i ++) {
+	navbarArray[i].addEventListener('click', function (e){
+		let sectionId = navbarArray[i].getAttribute('href');
+		console.log(sectionId);
 
-	let sectionOffset = document.querySelector(`${sectionId}`).offsetTop;
-	console.log(sectionOffset);
+		let sectionOffset = document.querySelector(`${sectionId}`).offsetTop;
+		console.log(sectionOffset);
 
-	e.preventDefault();
-	
-	scrollToSection();
+		e.preventDefault();
+		
+		scrollToSection();
 
-	function scrollToSection() {
-		html.scrollTop += 5;
-	
-		if(html.scrollTop < sectionOffset) {
-			setTimeout(scrollToSection, 2);
+		function scrollToSection() {
+			html.scrollTop += 5;
+		
+			if(html.scrollTop < sectionOffset) {
+				setTimeout(scrollToSection, 2);
+			}
 		}
-	}
-	
-});
+		
+	})
+};
