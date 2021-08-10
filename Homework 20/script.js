b8f3283034d336d9ea5395108b3075ed07735987
Ssosiashvili <sosiashvilisoso@gmail.com>
2021-08-11 02:59:01 +0400

    done

--- Homework 20/script.js
+++ Homework 20/script.js
@@ -1,62 +1,24 @@
-
-document.querySelectorAll('a')[4].addEventListener('click', function(e) {
-	console.log(this.getAttribute('href'));
-	scrollToSection();
-	e.preventDefault();
-})
-
-
-let sectionFourName = document.querySelectorAll('a')[4].getAttribute('href')
-
-let sectionFouroffset = document.querySelector(`${sectionFourName}`).offsetTop;
-let sectionOneScrollTop = document.querySelector(`${sectionFourName}`).scrollTop;
-
 let html = document.querySelector('html');
+let navbarArray = document.querySelectorAll('a');
 
+for (let i = 0; i < navbarArray.length; i ++) 
+navbarArray[i].addEventListener('click', function (e){
+	let sectionId = navbarArray[i].getAttribute('href');
+	console.log(sectionId);
 
-function scrollToSection() {
-	html.scrollTop += 5;
+	let sectionOffset = document.querySelector(`${sectionId}`).offsetTop;
+	console.log(sectionOffset);
 
-	if(html.scrollTop < sectionFouroffset) {
-		setTimeout(scrollToSection, 2);
+	e.preventDefault();
+	
+	scrollToSection();
+
+	function scrollToSection() {
+		html.scrollTop += 5;
+	
+		if(html.scrollTop < sectionOffset) {
+			setTimeout(scrollToSection, 2);
+		}
 	}
-}
-
-
-// function scrollToSection() {
-
-//  	html.scrollTop += 5;
-
-//  	if(html.scrollTop < sectionOneoffset) {
-//  		setTimeout(scrollToSection, 2);
-//  	}
-// }
-
-
-// let html = document.querySelector('html');
-// let sectionNames = document.querySelectorAll('a')
-// let sectionsTomove = document.querySelectorAll('section')
-
-
-// function listener () {
-//     for (let i = 0; i < sectionNames.length; i ++) {
-//         sectionNames[i].addEventListener ('click', function(e) {
-// 		scrollToSection();
-//         e.preventDefault();
-//         });
-//     }    
-// };
-
-
-
-// function scrollToSection (e) {
-// 	html.scrollTop += 5;
-// 	for (let i = 0; i < sectionsTomove.length; i++) {
-// 		let sectionOneName = document.querySelectorAll('a')[i].getAttribute('href');
-// 		let sectionOneoffset = document.querySelector(`${sectionOneName}`).offsetTop;
-// 		if (html.scrollTop < sectionOneoffset) { 
-// 			setTimeout(scrollToSection, 2);
-// 			e.preventDefault();
-// 		}
-// 	}
-// };
+	
+});
